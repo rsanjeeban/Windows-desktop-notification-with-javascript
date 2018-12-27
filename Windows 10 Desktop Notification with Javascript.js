e5ab7436679ac471fb634ef8232110a7db2be428
Your Name <your@email.com>
2018-12-27 15:57:09 +0530

    Windows 10 Desktop Notification with Javascript

---
+++ Windows 10 Desktop Notification with Javascript.js
@@ -0,0 +1,41 @@
+function notifyMe() {
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+    else if (Notification.permission === "granted") {
+        var options = {
+            body: "This is the body of the notification",
+            icon: "icon.jpg",
+            dir : "ltr"
+        };
+        var notification = new Notification("Hi there",options);
+    }
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+            if (!('permission' in Notification)) {
+                Notification.permission = permission;
+            }
+
+            if (permission === "granted") {
+                var options = {
+                    body: "This is the body of the notification",
+                    icon: "icon.jpg",
+                    dir : "ltr"
+                };
+                var notification = new Notification("Hi there",options);
+            }
+        });
+    }
+}
+
+Notification.requestPermission(function (permission) {
+    if (!('permission' in Notification)) {
+        Notification.permission = permission;
+    }
+});
+var options = {
+    body: message.notification_message,
+    icon: message.club_data.club_logo,
+    dir : "ltr"
+};
+var notification = new Notification('Driver', options); 
